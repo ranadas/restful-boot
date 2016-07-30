b8f59777919e1d19ec107353baf0986b0236dfda
Rana Das <rana.pratap.das@gmail.com>
2016-07-30 16:47:14 +0100

    Enabling Swagger

--- src/main/java/com/rdas/WebJsSpringBootApplication.java
+++ src/main/java/com/rdas/WebJsSpringBootApplication.java
@@ -3,10 +3,12 @@ package com.rdas;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by rdas on 29/07/2016.
  */
+@EnableSwagger2
 @Slf4j
 @SpringBootApplication
 public class WebJsSpringBootApplication {
